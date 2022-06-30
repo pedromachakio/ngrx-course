b36b9813a3c06a22c3ede21b05ffcf3da90f8a5d
Pedro <67802288+pedromachakio@users.noreply.github.com>
2022-06-30 13:42:19 +0000

    Implemented NgRx login action
    P.s. state still not being recorded on store
    
    Also formatted some code

---
+++ src/app/auth/auth.actions.ts
@@ -0,0 +1,9 @@
+import { createAction, props } from "@ngrx/store";
+import { User } from "./model/user.model";
+
+
+export const loginActionCreator = createAction(
+    "[Login Page] User Login", // Type of action following convention. [Source of action] Event the action corresponds to
+    props<{user: User}>() // payload
+)
+
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,41 +1,57 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-import {Store} from "@ngrx/store";
+import { Store } from "@ngrx/store";
 
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import { AuthService } from "../auth.service";
+import { tap } from "rxjs/operators";
+import { noop } from "rxjs";
+import { Router } from "@angular/router";
+import { GlobalAppState } from "../reducers";
+import { loginActionCreator } from "../auth.actions";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-
   form: FormGroup;
 
   constructor(
-      private fb:FormBuilder,
-      private auth: AuthService,
-      private router:Router) {
-
-      this.form = fb.group({
-          email: ['test@angular-university.io', [Validators.required]],
-          password: ['test', [Validators.required]]
-      });
-
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private store: Store<GlobalAppState>
+  ) {
+    this.form = formBuilder.group({
+      email: ["test@angular-university.io", [Validators.required]],
+      password: ["test", [Validators.required]],
+    });
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   login() {
+    const formValue = this.form.value;
 
+    this.authService
+      .login(formValue.email, formValue.password) // returns an Observable, so we gotta subscribe to it
+
+      .pipe(
+        tap((user) => {
+          // tap allows us to create side effects, como log & store User info on NgRx, e redirect to /courses, in this case
+
+          console.log(user);
+
+          this.store.dispatch(loginActionCreator({ user })); // dispatching actions is the way to modify the store state; allows 4 loose coupling
+
+          this.router.navigateByUrl("/courses");
+        })
+      )
+      .subscribe(
+        noop, // if everything goes to plan, no operation
+        () => alert("Login failed") // otherwise handle errors
+      );
   }
-
 }
-
