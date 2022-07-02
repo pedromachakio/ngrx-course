80e2f0913e317f922cef11161377b1c3f44ffd34
Pedro <67802288+pedromachakio@users.noreply.github.com>
2022-07-02 21:42:55 +0000

    Better implementation of NgRx Effects for login action

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,16 +1,37 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { dispatch } from "rxjs/internal/observable/pairs";
+import { tap } from "rxjs/operators";
+import { AuthActions } from "./action-types";
 
 @Injectable()
 export class AuthEffects {
   // we want to as a side effect save the user profile in the local storage after a (login) action
 
-  constructor(private action$: Actions) {
-    // not the best way to implement it, not typesafe, just a basic example
-    action$.subscribe((action) => {
-      if (action.type == "[Login Page] User Login") { // wait for login action to be emitted 
-        localStorage.setItem("user", JSON.stringify(action["user"])); // as a side effect of that action, save the user to local storage
-      }
-    });
-  }
+  constructor(private action$: Actions) {}
+
+  login$ = createEffect(() =>
+    this.action$.pipe(
+      ofType(AuthActions.loginActionCreator), // gives auto completion
+      tap((action) => localStorage.setItem("user", JSON.stringify(action.user)))
+    )
+  , {dispatch: false}); // to let ngrx effects know that this effect doesnt result in dispatching an action
 }
+
+//  not the best way to implement it, not typesafe, just a basic example
+// action$.subscribe((action) => {
+//  if (action.type == "[Login Page] User Login") {
+//    wait for login action to be emitted
+//    localStorage.setItem("user", JSON.stringify(action["user"]));  as a side effect of that action, save the user to local storage
+//
+//
+
+// better way to filter for actions of login type with ngrx
+// const login$ = this.action$.pipe(
+//  ofType(AuthActions.loginActionCreator),
+//  tap((loginAction) => {
+//    localStorage.setItem("user", JSON.stringify(loginAction.user)); now we can write it in a type safe way
+//  })  add side effect with tap
+//   );
+// and now we subscribe only to the observable of login actions, instead of an observable of all actions, as was done previously above
+//login$.subscribe();  to add "automatic" error handling and avoid subscribing manually we have changed this and used the createEffect method
