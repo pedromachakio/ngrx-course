9d023c32146a5db2b00d95f6ae5c5e1b5624b368
Pedro <67802288+pedromachakio@users.noreply.github.com>
2022-07-03 20:03:40 +0000

    Implemented sorting with sortcomparer

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -3,11 +3,14 @@ import { createReducer, createSelector, on } from "@ngrx/store";
 
 import { CourseActions } from "../action-types";
 import { selectAllCourses } from "../courses.selectors";
-import { Course } from "../model/course";
+import { compareCourses, Course } from "../model/course";
 
 export interface CoursesState extends EntityState<Course> {}
 
-export const adapter = createEntityAdapter<Course>(); // facilita crud operations com Entity
+export const adapter = createEntityAdapter<Course>({
+  sortComparer: compareCourses  // compares 2 course entitites to sort them
+  // would have to use selectId here ifproperty in course model was not "id"
+}); // facilita crud operations com Entity
 
 export const initialCoursesState = adapter.getInitialState();
 
